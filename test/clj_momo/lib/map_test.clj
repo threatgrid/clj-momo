4d2ee8838ce9dfa3c04508e08585e3f930034067
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2016-09-15 16:38:26 +0200

    added tests

--- test/clj_momo/lib/map_test.clj
+++ test/clj_momo/lib/map_test.clj
@@ -10,3 +10,11 @@
 
 (deftest test-keys-in-all
   (is false))
+
+(deftest test-assoc-if
+  (is {:a 10 :c 3}
+      (sut/assoc-if #(< (count (str %1 %2)) 5) {} :a 1 :waytoolong 2 :c 3)))
+
+(deftest test-assoc-some
+  (is {:a 10 :c 3}
+      (sut/assoc-some {} :a 1 :b nil :c 3)))

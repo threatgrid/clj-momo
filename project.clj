58a2886707afdd2d24097f1c43d927178f09d056
Stephen Sloan <steve@stevesloan.com>
2017-09-26 09:52:07 -0700

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/clj-momo "0.2.12"
+(defproject threatgrid/clj-momo "0.2.13-SNAPSHOT"
   :description "Library code produced by the Cisco ThreatGrid team for building swagger backed API services"
   :url "https://github.com/threatgrid/clj-momo"
   :license {:name "Eclipse Public License"

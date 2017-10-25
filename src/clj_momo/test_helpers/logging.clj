7cec18dffe934a8f0413d78a01190e6baf6910aa
Stephen Sloan <steve@stevesloan.com>
2017-10-25 14:39:31 -0700

    Improving the with-test-logging helper (#30)

--- src/clj_momo/test_helpers/logging.clj
+++ src/clj_momo/test_helpers/logging.clj
@@ -3,29 +3,47 @@
    [clojure.tools.logging :refer [*logger-factory*]]
    [clojure.tools.logging.impl :as impl]))
 
-;; ----- Logging test helpers coming from the tools.logging test code
+;; ----- Logging test helpers based on code from the tools.logging test code
 ;; See https://github.com/clojure/tools.logging/blob/master/src/test/clojure/clojure/tools/test_logging.clj for usage
 
-(defn test-factory [enabled-set entries-atom agent-used-atom]
-  (let [main-thread (Thread/currentThread)]
+(defn get-current-thread []
+  (Thread/currentThread))
+
+(defn test-factory [enabled-set
+                    last-log-entry-atom
+                    agent-used-atom
+                    all-log-entries-atom]
+  (let [main-thread (get-current-thread)]
     (reify impl/LoggerFactory
       (name [_] "test factory")
       (get-logger [_ log-ns]
         (reify impl/Logger
           (enabled? [_ level] (contains? enabled-set level))
           (write! [_ lvl ex msg]
-            (reset! entries-atom [(str log-ns) lvl ex msg])
-            (reset! agent-used-atom (not (identical? main-thread (Thread/currentThread))))))))))
+            (let [log-data [(str log-ns) lvl ex msg]]
+              (swap! all-log-entries-atom (fnil conj []) log-data)
+              (reset! last-log-entry-atom log-data))
+            (reset! agent-used-atom
+                    (not (identical? main-thread (get-current-thread))))))))))
 
 (defmacro with-test-logging
-  [[enabled-level-set log-entry-sym agent-used?-sym] & body]
+  [[enabled-level-set last-log-entry-sym agent-used?-sym all-log-entries-sym] & body]
   (let [enabled-level-set (or enabled-level-set #{:trace :debug :info :warn :error :fatal})
-        log-entry-sym (or log-entry-sym 'log-entry-sym)
-        agent-used?-sym (or agent-used?-sym 'agent-used?-sym)]
-    `(let [~log-entry-sym (atom nil)
-           ~agent-used?-sym (atom nil)]
+        last-log-entry-sym (or last-log-entry-sym 'last-log-entry-sym)
+        agent-used?-sym (or agent-used?-sym 'agent-used?-sym)
+        all-log-entries-sym (or all-log-entries-sym 'all-log-entries-sym)]
+    `(let [~last-log-entry-sym (atom nil)
+           ~agent-used?-sym (atom nil)
+           ~all-log-entries-sym (atom nil)]
        (binding [*logger-factory* (test-factory
                                     ~enabled-level-set
-                                    ~log-entry-sym
-                                    ~agent-used?-sym)]
+                                    ~last-log-entry-sym
+                                    ~agent-used?-sym
+                                    ~all-log-entries-sym)]
          ~@body))))
+
+
+(defn was-logged? [log-regex all-log-entries-atom]
+  (boolean
+   (some #(re-find log-regex %)
+         (map #(nth % 3) @all-log-entries-atom))))

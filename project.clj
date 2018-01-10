7df82dcc284aa50d6c8b96b7a204f36bd3a91d5d
Paula Gearon <pgearon@cisco.com>
2018-01-10 18:45:06 -0500

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -7,29 +7,29 @@
                  [org.clojure/tools.logging "0.3.1"]
 
                  ;; schemas
-                 [prismatic/schema "1.1.2"]
-                 [metosin/schema-tools "0.9.0"]
+                 [prismatic/schema "1.1.7"]
+                 [metosin/schema-tools "0.9.1"]
 
                  ;; time
-                 [com.andrewmcveigh/cljs-time "0.5.0"
+                 [com.andrewmcveigh/cljs-time "0.5.2"
                   :exclusions [org.clojure/clojurescript]]
-                 [clj-time "0.13.0"]
+                 [clj-time "0.14.2"]
 
                  ;; url
                  [com.cemerick/url "0.1.1"
                   :exclusions [org.clojure/clojurescript]]
 
                  ;;json
-                 [cheshire "5.6.3"]
+                 [cheshire "5.8.0"]
 
                  ;;http
-                 [clj-http "3.4.1"]
+                 [clj-http "3.7.0"]
 
                  ;; Metrics
-                 [metrics-clojure "2.9.0"]
-                 [metrics-clojure-jvm "2.9.0"]
-                 [metrics-clojure-ring "2.9.0"]
-                 [metrics-clojure-riemann "2.9.0"]
+                 [metrics-clojure "2.10.0"]
+                 [metrics-clojure-jvm "2.10.0"]
+                 [metrics-clojure-ring "2.10.0"]
+                 [metrics-clojure-riemann "2.10.0"]
                  [clout "2.1.2"]
                  [slugger "1.0.1"]
                  [riemann-clojure-client "0.4.5"
@@ -62,6 +62,6 @@
                    :default (complement :integration)
                    :all (constantly true)}
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/clojurescript "1.9.521"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/clojurescript "1.9.946"]]
                    :resource-paths ["test/resources"]}})

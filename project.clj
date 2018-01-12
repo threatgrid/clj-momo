cab62fcba945b72b46ab7ebf37517fcf68ce9f97
Paula Gearon <pgearon@cisco.com>
2018-01-12 13:52:53 -0500

    Release 0.2.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/clj-momo "0.2.17-SNAPSHOT"
+(defproject threatgrid/clj-momo "0.2.17"
   :description "Library code produced by the Cisco ThreatGrid team for building swagger backed API services"
   :url "https://github.com/threatgrid/clj-momo"
   :license {:name "Eclipse Public License"

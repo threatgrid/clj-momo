6cf2a6a6cd5959fccdca351eb83f2b949aca93e1
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2018-04-13 10:35:25 +0200

    Bump to 0.2.22-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/clj-momo "0.2.21"
+(defproject threatgrid/clj-momo "0.2.22-SNAPSHOT"
   :description "Library code produced by the Cisco ThreatGrid team for building swagger backed API services"
   :url "https://github.com/threatgrid/clj-momo"
   :license {:name "Eclipse Public License"

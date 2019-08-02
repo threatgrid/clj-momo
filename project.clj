cfd75a52bcf636d91476224aac73f7e4e429d324
Guillaume ERETEO <gereteo@cisco.com>
2019-08-02 15:05:51 +0200

    0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/clj-momo "0.3.0-SNAPSHOT"
+(defproject threatgrid/clj-momo "0.3.1-SNAPSHOT"
   :description "Library code produced by the Cisco ThreatGrid team for building swagger backed API services"
   :url "https://github.com/threatgrid/clj-momo"
   :license {:name "Eclipse Public License"

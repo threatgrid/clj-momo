3998df4526dc9113a509689358edc58619be2fb0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-02-12 18:50:11 -0500

    Turn on Lein :pedantic? and fix warnings (#67)
    * fix dependency warnings
    
    * restore previous schema deps order
    
    * fix uri lib and add tests
    
    * compilation sanity check in CI
    
    * use tg metrics clojure library

--- project.clj
+++ project.clj
@@ -1,46 +1,47 @@
+(def clj-version "1.10.1")
+(def metrics-clojure-version "2.10.0")
+
 (defproject threatgrid/clj-momo "0.3.4-SNAPSHOT"
   :description "Library code produced by the Cisco ThreatGrid team for building swagger backed API services"
   :url "https://github.com/threatgrid/clj-momo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;; logging
+  :pedantic? :abort
+  ; when changing, use `lein pom; mvn dependency:tree -Dverbose` to print
+  ; full deps tree and understand all conflicts.
+  ; Also see `https://github.com/clj-commons/vizdeps` for visual representation
+  ; (may require a local install, not on clojars).
+  ; Use :exclusions only to permanently delete a dependency, and document reasons.
+  ; Avoid combining :exclusions with an override as it can obscure the indended
+  ; dependencies when browsing the full deps tree.
+  :dependencies [[org.clojure/clojure ~clj-version]
                  [org.clojure/tools.logging "0.5.0"]
-
-                 ;; schemas
                  [prismatic/schema "1.1.12"]
                  [metosin/schema-tools "0.12.2"]
-
-                 ;; time
                  [com.andrewmcveigh/cljs-time "0.5.2"]
                  [clj-time "0.15.2"]
-
-                 ;; uri
                  [com.arohner/uri "0.1.2"]
-
-                 ;;json
                  [cheshire "5.9.0"]
-
-                 ;;http
-                 [clj-http "3.10.0"]
-
-                 ;; Metrics
-                 [metrics-clojure "2.10.0"]
-                 [metrics-clojure-jvm "2.10.0"]
-                 [metrics-clojure-ring "2.10.0"]
-                 [metrics-clojure-riemann "2.10.0"]
-                 [clout "2.2.1"]
+                 [clj-http "3.10.0"] ;TODO bump clj-http with https://github.com/dakrone/clj-http/pull/532
                  [slugger "1.0.1"]
-                 [riemann-clojure-client "0.5.1"
-                  ;; Protobuf-java is brought in by ClojureScript
-                  :exclusions [com.google.protobuf/protobuf-java]]]
+                 [riemann-clojure-client "0.5.1"]
+                 [metrics-clojure ~metrics-clojure-version]
+                 [metrics-clojure-jvm ~metrics-clojure-version]
+                 [metrics-clojure-ring ~metrics-clojure-version]
+                 [threatgrid/metrics-clojure-riemann "2.10.1"] ; uses io.riemann.* classes instead of com.aphyr
+                 [clout "2.2.1"]]
 
   :main nil
 
   :codox {:output-path "doc"
           :source-paths ["src"]}
 
-  :plugins [[lein-cljsbuild "1.1.7"]
+  ; use `lein deps :plugin-tree` to debug
+  :plugins [[org.clojure/clojure ~clj-version] ;update lein-cljsbuild and lein-doo's dep
+            [lein-cljsbuild "1.1.7"]
             [lein-codox "0.10.7"]
+            [com.google.guava/guava "20.0"] ;fix some unknown internal conflict in `doo`
+            [doo "0.1.11"] ;update lein-doo's dep
             [lein-doo "0.1.10"]]
 
   :cljsbuild {:builds {:node {:source-paths ["src" "test"]
@@ -64,9 +65,10 @@
 
   :global-vars {*warn-on-reflection* true}
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [org.clojure/clojurescript "1.10.597"
-                                   :exclusions [com.google.errorprone/error_prone_annotations
-                                                com.google.code.findbugs/jsr305]]
+  :profiles {:dev {:dependencies [;https://clojure.atlassian.net/browse/CLJS-3047
+                                  [com.google.errorprone/error_prone_annotations "2.1.3"]
+                                  ;https://clojure.atlassian.net/browse/CLJS-3047
+                                  [com.google.code.findbugs/jsr305 "3.0.2"]
+                                  [org.clojure/clojurescript "1.10.597"]
                                   [ch.qos.logback/logback-classic "1.2.3"]]
                    :resource-paths ["test/resources"]}})

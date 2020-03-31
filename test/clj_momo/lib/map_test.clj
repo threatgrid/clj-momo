60a25a38e1f97e54b39edbc03ebe30fbd126bca6
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2020-03-31 15:42:32 +0200

    more test cases

--- test/clj_momo/lib/map_test.clj
+++ test/clj_momo/lib/map_test.clj
@@ -1,6 +1,6 @@
 (ns clj-momo.lib.map-test
   (:require [clj-momo.lib.map :as sut]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is are testing]]))
 
 (deftest test-assoc-if
   (is {:a 10 :c 3}
@@ -31,4 +31,24 @@
   (is (= {:a {:c :d}}
          (sut/deep-merge {:a :b} {:a {:c :d}})))
   (is (= {:a :b}
-         (sut/deep-merge {:a {:c :d}} {:a :b}))))
+         (sut/deep-merge {:a {:c :d}} {:a :b})))
+  (is (= {:a {:b {:x :y}}}
+         (sut/deep-merge {:a {:b :c}}
+                         nil
+                         {:a nil}
+                         {:a {:b {:x :y}}})))
+  (is (= {:a :b} (sut/deep-merge {:a :b})))
+  (testing "m1 is a hashmap and m2 is different"
+    (are [x m2] (= x
+                   (sut/deep-merge {:a :b}  m2))
+      {:a :b} nil
+      :c      :c
+      {:a :b
+       :c :d} {:c :d}))
+  (testing "m2 is a hashmap and m1 is different"
+    (are [x m1] (= x
+                   (sut/deep-merge m1 {:a :b}))
+      {:a :b} nil
+      {:a :b} :c
+      {:a :b
+       :c :d} {:c :d})))

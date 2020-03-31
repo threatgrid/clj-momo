2b08b40536be5028cd69ee260b23b16b51a8fd7c
Matthieu Sprunck <msprunck@cisco.com>
2020-03-31 16:11:37 +0200

    Fix deep-merge when the last map is nil (#73)
    `(deep-merge {:a :b} nil) => nil`
    
    it should return `{:a :b}`
    
    This PR fixes it. I found the solution in this stackoverflow comment https://stackoverflow.com/a/27214324

--- test/clj_momo/lib/map_test.clj
+++ test/clj_momo/lib/map_test.clj
@@ -1,6 +1,6 @@
 (ns clj-momo.lib.map-test
   (:require [clj-momo.lib.map :as sut]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is are testing]]))
 
 (deftest test-assoc-if
   (is {:a 10 :c 3}
@@ -22,4 +22,58 @@
               :ab "ok"
               :ac {:aca "ok"
                    :acb "ok"
-                   :acc "ok"}}})))
+                   :acc "ok"}}}))
+  (is (= {:a :b} (sut/deep-merge nil {:a :b})))
+  (is (= {:a :b} (sut/deep-merge {:a :b} nil)))
+  (is (nil? (sut/deep-merge nil nil)))
+  (is (= {:a {:b :c}}
+         (sut/deep-merge {:a {:b :c}} {:a nil})))
+  (is (= {:a {:c :d}}
+         (sut/deep-merge {:a :b} {:a {:c :d}})))
+  (is (= {:a :b}
+         (sut/deep-merge {:a {:c :d}} {:a :b})))
+  (is (= {:a {:b {:x :y}}}
+         (sut/deep-merge {:a {:b :c}}
+                         nil
+                         {:a nil}
+                         {:a {:b {:x :y}}})))
+  (is (= {:c1 {:a :b}
+          :c2 {:a :b}
+          :c3 :c3
+          :c4 {:a :b :x :y}
+          :d1 :v
+          :d2 :v
+          :d3 :d3
+          :d4 {:x :y}
+          :e1 nil
+          :e2 nil
+          :e3 :e3
+          :e4 {:x :y}
+          :f2 nil
+          :f3 :f3
+          :f4 {:x :y}}
+         (sut/deep-merge
+          {:c1 {:a :b}
+           :c2 {:a :b}
+           :c3 {:a :b}
+           :c4 {:a :b}
+           :d1 :v
+           :d2 :v
+           :d3 :v
+           :d4 :v
+           :e1 nil
+           :e2 nil
+           :e3 nil
+           :e4 nil}
+          {:c2 nil
+           :c3 :c3
+           :c4 {:x :y}
+           :d2 nil
+           :d3 :d3
+           :d4 {:x :y}
+           :e2 nil
+           :e3 :e3
+           :e4 {:x :y}
+           :f2 nil
+           :f3 :f3
+           :f4 {:x :y}}))))
